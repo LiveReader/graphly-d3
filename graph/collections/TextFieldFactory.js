ff36ca3fa52679477397a6e1773e6dff3205aa08
Jason Rietzke <jason.rietzke@livereader.com>
2021-12-21 20:46:51 +0100

    implemented: TextFieldFactory

---
+++ graph/collections/TextFieldFactory.js
@@ -0,0 +1,51 @@
+/**
+ * @param  {Object} parent D3 parent object
+ * @param  {string} text the text within the tag
+ * @param  {ShapeStyle[]} styles the styles for the tag
+ * @return {Object} shape
+ */
+function Text(parent, text, styles = []) {
+	const shape = parent.append("text");
+	styles.forEach((style) => {
+		shape.classed(style.className, (d) => style.condition(d));
+	});
+	shape.text(text);
+	shape.remove();
+	return shape.node();
+}
+
+class TextFieldFactory extends CollectionFactory {
+	/**
+	 * @callback onElement
+	 * @param    {object} data D3 data object
+	 * @return   {string} text to be rendered
+	 */
+	/**
+	 * @param  {Object} shape d3 data object
+	 * @param  {CollectionStyle} collectionStyle the collection style
+	 * @param  {ShapeStyle[]} textStyles the styles for the text
+	 * @callback onElement onElement function to be called on each element created on data
+	 */
+	constructor(
+		shape,
+		collectionStyle,
+		textStyles,
+		onElement = (el) => {
+			return "";
+		}
+	) {
+		super(collectionStyle);
+
+		shape.select((d) => {
+			const currentNode = shape.filter((el) => el.id === d.id);
+			currentNode.select((el) => {
+				const text = onElement(el);
+				const words = text.split(" ");
+				words.forEach((word) => {
+					const textElement = Text(currentNode, word, textStyles);
+					super.addItem(textElement, currentNode);
+				});
+			});
+		});
+	}
+}
