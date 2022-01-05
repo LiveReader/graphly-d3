9512190897c985b989a3fe287fc8e77ba705a51a
Jason Rietzke <jason.rietzke@livereader.com>
2022-01-05 21:20:59 +0100

    fixed: TagShape background path

---
+++ graph/functionals/collections/TagCollection.js
@@ -0,0 +1,16 @@
+/**
+ * @param  {String[]} tags array of strings
+ * @param  {CollectionStyle} style collection style
+ * @param  {TagStyle} tagStyle tag style
+ * @return {Object} shape
+ */
+function TagCollection(tags, style, tagStyle) {
+	const tagShapes = [];
+	tags.forEach((tag) => {
+		const tagShape = TagShape(tag, tagStyle);
+		tagShapes.push(tagShape);
+	});
+	const ellipsis = TagShape("...", tagStyle);
+	const collection = ShapeCollection(tagShapes, style, ellipsis);
+	return collection;
+}
--- graph/functionals/shapes/TagShape.js
+++ graph/functionals/shapes/TagShape.js
@@ -3,7 +3,6 @@
  * @param  {String[]} textStyles array of css classes
  * @param  {String[]} backgroundStyles array of css classes
  * @param  {Number} cornerRadius radius of the corners
- * @return {Object} style
  */
 function TagStyle(padding = [0, 0], textStyles = [], backgroundStyles = [], cornerRadius = 0) {
 	return {
@@ -17,41 +16,40 @@ function TagStyle(padding = [0, 0], textStyles = [], backgroundStyles = [], corn
 /**
  * @param  {String} text text of tag
  * @param  {TagStyle} style of the tag
+ * @return {Object} shape
  */
 function TagShape(text, style) {
 	const shape = Shape.create("g").classed("tag", true);
 
-	const textShape = shape.append("text").text(text).attr("dy", "0.5em");
+	const textShape = shape.append("text").text(text).attr("dy", "0.35em");
 	style.textStyles.forEach((style) => {
 		textShape.classed(style, true);
 	});
-	const textBBox = Shape.getBBox(textShape);
+	const textBBox = Shape.getBBox(shape);
 
+	const width = textBBox.width + style.padding[0] * 2;
+	const height = textBBox.height + style.padding[1] * 2;
+	const cr = style.cornerRadius;
 	const backgroundShape = shape
 		.append("path")
 		.attr(
 			"d",
-			`M ${textBBox.width / 2 - style.padding[0]} ${-style.padding[1]} ` +
-				`L ${textBBox.width + style.padding[0] - style.cornerRadius} ${-style.padding[1]} ` +
-				`A ${style.cornerRadius} ${style.cornerRadius} 0 0 1 ${textBBox.width + style.padding[0]} ${
-					-style.padding[1] + style.cornerRadius
-				} ` +
-				`L ${textBBox.width + style.padding[0]} ${textBBox.height + style.padding[1] - style.cornerRadius} ` +
-				`A ${style.cornerRadius} ${style.cornerRadius} 0 0 1 ${
-					textBBox.width + style.padding[0] - style.cornerRadius
-				} ${textBBox.height + style.padding[1]} ` +
-				`L ${-style.padding[0] + style.cornerRadius} ${textBBox.height + style.padding[1]} ` +
-				`A ${style.cornerRadius} ${style.cornerRadius} 0 0 1 ${-style.padding[0]} ${
-					textBBox.height + style.padding[1] - style.cornerRadius
-				} ` +
-				`L ${-style.padding[0]} ${-style.padding[1] + style.cornerRadius} ` +
-				`A ${style.cornerRadius} ${style.cornerRadius} 0 0 1 ${-style.padding[0] + style.cornerRadius} ${-style
-					.padding[1]} ` +
+			`M ${0} ${-height / 2} ` +
+				`L ${width / 2 - cr} ${-height / 2} ` +
+				`A ${cr} ${cr} 0 0 1 ${width / 2} ${-(height / 2) + cr} ` +
+				`L ${width / 2} ${height / 2 - cr} ` +
+				`A ${cr} ${cr} 0 0 1 ${width / 2 - cr} ${height / 2} ` +
+				`L ${-width / 2 + cr} ${height / 2} ` +
+				`A ${cr} ${cr} 0 0 1 ${-width / 2} ${height / 2 - cr} ` +
+				`L ${-width / 2} ${-height / 2 + cr} ` +
+				`A ${cr} ${cr} 0 0 1 ${-width / 2 + cr} ${-height / 2} ` +
 				`Z`
 		);
 	style.backgroundStyles.forEach((style) => {
 		backgroundShape.classed(style, true);
 	});
 
+	textShape.remove();
+	shape.append(() => textShape.node());
 	return shape;
 }
