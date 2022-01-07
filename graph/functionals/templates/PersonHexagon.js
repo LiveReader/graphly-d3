e18ac18130949a3eb9c6a048ff1251600c8de86e
Jason Rietzke <jason.rietzke@livereader.com>
2022-01-07 18:36:18 +0100

    refactored: PersonHexagon buildings steps

--- graph/functionals/templates/PersonHexagon.js
+++ graph/functionals/templates/PersonHexagon.js
@@ -2,36 +2,36 @@ function PersonHexagon(data, initialShape, changes) {
 	const shape = initialShape ? initialShape : Shape.create("g");
 	// shape.selectAll("*").remove();
 
-	addPaths();
-	addTags();
-	addTitle();
+	const bodyShape = addBody();
+	const headShape = addHead();
+	const tagCollection = addTags();
+	const titleShape = addTitle();
+	const largeTitleShape = addLargeTitle();
 
-	if (!initialShape) {
-		Shape.resize(shape, data.shape.scale * 300);
-	}
+
+	Shape.resize(shape, data.shape.scale * 300);
 
 	return shape;
 
-	function addPaths() {
+	function addBody() {
 		const bodyShape = initialShape
 			? shape.select("path.background")
 			: PathShape(
 					"M268.62,884a31,31,0,0,1-26.76-15.45L4.64,457.72a31,31,0,0,1,0-30.9L241.86,16A31,31,0,0,1,268.62.5H743.05A31,31,0,0,1,769.81,16L1007,426.82a31,31,0,0,1,0,30.9L769.81,868.59A31,31,0,0,1,743.05,884Z"
 			  );
+		bodyShape.classed("hexagon-person", true).classed("shadow", true);
+		if (!initialShape) {
+			shape.append(() => bodyShape.node());
+		}
+		return bodyShape;
+	}
 
-		bodyShape.classed("hexagon-person", true).classed("background", true).classed("shadow", true);
-		// .classed("status", true)
-		// .classed("deceased", true)
-		// .classed("immediate", true)
-		// .classed("delayed", true)
-		// .classed("minor", true)
-
+	function addHead() {
 		const headShape = initialShape
 			? shape.select("path.status")
 			: PathShape(
 					"M506.08,253.5C643,253.5,774.2,246.6,895.77,234L770.34,16.7A31.4,31.4,0,0,0,743.15,1H268.71a31.38,31.38,0,0,0-27.19,15.7L116.1,233.93C237.75,246.59,369.09,253.5,506.08,253.5Z"
 			  );
-
 		headShape
 			.classed("hexagon-person", true)
 			.classed("status", true)
@@ -39,15 +39,14 @@ function PersonHexagon(data, initialShape, changes) {
 			.classed("immediate", data.status === "immediate")
 			.classed("delayed", data.status === "delayed")
 			.classed("minor", data.status === "minor");
-
 		if (!initialShape) {
-			shape.append(() => bodyShape.node());
 			shape.append(() => headShape.node());
 		}
+		return headShape;
 	}
 
 	function addTags() {
-		if (!(changes.tags || changes.status)) return;
+		if (!(changes.tags || changes.status)) return shape.select("g.tags");
 		shape.select("g.tags").remove();
 		const bbox = Shape.getBBox(shape);
 		const tagCollection = TagCollection(
@@ -70,21 +69,37 @@ function PersonHexagon(data, initialShape, changes) {
 		);
 		tagCollection.classed("tags", true);
 		shape.append(() => tagCollection.node());
+		return tagCollection;
 	}
 
 	function addTitle() {
-		console.log(changes.name);
-		if (!changes.name) return;
+		if (!changes.name) return shape.select("g.title");
 		shape.select("g.title").remove();
 		const bbox = Shape.getBBox(shape);
 		const initials = data.name.first.charAt(0) + data.name.last.charAt(0);
-		const title = TextCollection(
+		const titleShape = TextCollection(
 			initials,
 			CollectionStyle(100, bbox.width, 0, bbox.height * 0.47, 40, 40, 1, Alignment.Center, [60]),
 			[ShapeStyle("hexagon-person", true), ShapeStyle("title", true)]
 		);
-		title.classed("title", true);
-		shape.append(() => title.node());
+		titleShape.classed("title", true);
+		shape.append(() => titleShape.node());
+		return titleShape;
+	}
+
+	function addLargeTitle() {
+		if (!changes.name) return shape.select("g.largeTitle");
+		shape.select("g.largeTitle").remove();
+		const bbox = Shape.getBBox(shape);
+		const initials = data.name.first.charAt(0) + data.name.last.charAt(0);
+		const largeTitleShape = TextCollection(
+			initials,
+			CollectionStyle(100, bbox.width, 0, bbox.height * 0.65, 40, 40, 1, Alignment.Center, [60]),
+			[ShapeStyle("hexagon-person", true), ShapeStyle("title", true), ShapeStyle("large", true)]
+		);
+		largeTitleShape.classed("largeTitle", true);
+		shape.append(() => largeTitleShape.node());
+		return largeTitleShape;
 	}
 }
 
