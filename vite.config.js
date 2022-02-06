173b0857e33a258d29a1af416f22e20fc6f41587
Jason Rietzke <jason.rietzke@livereader.com>
2022-02-06 18:32:21 +0100

    updated: vite config
    excluded d3 from build

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,13 @@ module.exports = defineConfig({
 			name: "Graphly D3",
 			fileName: (format) => `main.${format}.js`,
 		},
+		rollupOptions: {
+			external: ["d3"],
+			output: {
+				globals: {
+					d3: "d3",
+				},
+			},
+		},
 	},
 });
