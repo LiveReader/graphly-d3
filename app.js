a2d06ed85adde888a63d51954ae604c0a7d20c77
Jason Rietzke <jason.rietzke@livereader.com>
2022-02-09 15:15:06 +0100

    updated: demo templates url

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let graph = {
 };
 
 const simulation = new ForceSimulation(svg);
-simulation.setTemplateOrigin("http://localhost:3001/templates/");
+simulation.setTemplateOrigin("http://" + document.location.host + "/templates/");
 
 simulation.onClick((e, d) => {
 	graph.nodes.forEach((node) => {
