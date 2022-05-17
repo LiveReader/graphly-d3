0fb5067c2fc8d0d1cda1563379c46465a34d254c
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-17 16:42:45 +0200

    prepared: custom layout

---
+++ docs/.vitepress/theme/GraphlyLayout.vue
@@ -0,0 +1,6 @@
+<template></template>
+
+<script setup>
+import DefaultTheme from "vitepress/theme";
+const { Layout } = DefaultTheme;
+</script>
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,7 @@
+import DefaultTheme from "vitepress/theme";
+import GraphlyLayout from "./GraphlyLayout.vue";
+
+export default {
+	...DefaultTheme,
+	Layout: GraphlyLayout,
+};
