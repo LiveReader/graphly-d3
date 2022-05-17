f975a6800289270df048eae51773026ff51bae76
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-17 16:42:17 +0200

    added: config

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+module.exports = {
+	lang: "en-US",
+	title: "Graphly D3 Docs",
+	description: "Documentation for the Graphly D3 library",
+	themeConfig: {
+		repo: "livereader/graphly-d3-docs",
+		docsDir: "docs",
+		docsBranch: "main",
+		editLinks: true,
+		editLinkText: "Edit this page on GitHub",
+		lastUpdated: "Last Updated",
+
+		nav: [],
+		sidebar: {},
+	},
+};
