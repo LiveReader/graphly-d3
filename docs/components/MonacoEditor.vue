5206570a11cdafe7ed10a69151e854ae50adcc2a
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-20 12:10:06 +0200

    fixed: MonacoEditor + set workers

--- docs/components/MonacoEditor.vue
+++ docs/components/MonacoEditor.vue
@@ -4,7 +4,48 @@
 
 <script setup>
 import { ref, watch, onMounted, defineProps, defineEmits } from "vue";
-import * as monaco from "monaco-editor";
+
+let monaco = null;
+
+async function init() {
+	monaco.editor.defineTheme("light", editorLightTheme);
+	monaco.editor.defineTheme("dark", editorDarkTheme);
+
+	self.MonacoEnvironment = {
+		getWorkerUrl: function(moduleId, label) {
+			if (label === "json") {
+				return "/monacoeditorwork/json.worker.bundle.js";
+			}
+			if (label === "css") {
+				return "/monacoeditorwork/css.worker.bundle.js";
+			}
+			if (label === "html") {
+				return "/monacoeditorwork/html.worker.bundle.js";
+			}
+			if (label === "typescript" || label === "javascript") {
+				return "/monacoeditorwork/ts.worker.bundle.js";
+			}
+			return "/monacoeditorwork/editor.worker.bundle.js";
+		}
+	};
+
+	const editor = monaco.editor.create(document.getElementsByClassName("monacoEditor")[0], {
+		value: props.editorContent,
+		language: props.editorLanguage,
+		automaticLayout: true,
+		theme: theme.value
+	});
+
+	editor.onDidChangeModelContent(() => {
+		emits("editorContentChange", editor.getValue());
+	});
+
+	const themeObserver = new MutationObserver(() => {
+		theme.value = document.getElementsByTagName("html")[0].getAttribute("color-mode");
+		monaco.editor.setTheme(theme.value);
+	});
+	themeObserver.observe(document.getElementsByTagName("html")[0], { attributes: true });
+}
 
 const props = defineProps({
 	editorLanguage: {
@@ -39,37 +80,29 @@ const editorDarkTheme = {
 	}
 };
 
-onMounted(() => {
-	monaco.editor.defineTheme("light", editorLightTheme);
-	monaco.editor.defineTheme("dark", editorDarkTheme);
-
-	const editor = monaco.editor.create(document.getElementsByClassName("monacoEditor")[0], {
-		value: props.editorContent,
-		language: props.editorLanguage,
-		automaticLayout: true,
-		theme: theme.value,
-	});
-
-	editor.onDidChangeModelContent(() => {
-		emits("editorContentChange", editor.getValue());
-	});
-
-	const themeObserver = new MutationObserver(() => {
-		theme.value = document.getElementsByTagName("html")[0].getAttribute("color-mode");
-		monaco.editor.setTheme(theme.value);
-	});
-	themeObserver.observe(document.getElementsByTagName("html")[0], { attributes: true });
+onMounted(async () => {
+	theme.value = document.getElementsByTagName("html")[0].getAttribute("color-mode");
+	if (!monaco) {
+		monaco = await import("monaco-editor");
+		setTimeout(async () => {
+			init();
+		}, 100);
+	} else {
+		init();
+	}
 });
 
 watch(
 	() => props.editorContent,
 	newValue => {
+		if (monaco == null) return;
 		editor.setValue(newValue);
 	}
 );
 watch(
 	() => props.editorLanguage,
 	newValue => {
+		if (monaco == null) return;
 		editor.setModel(monaco.editor.createModel(props.editorContent, newValue));
 	}
 );
