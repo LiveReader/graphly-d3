d26a928e1195e498a7d8d995e5ccce5cb79fc915
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-28 02:01:21 +0200

    updated: dependency versions

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "lib"),
+		},
+	},
 	build: {
 		lib: {
 			entry: resolve(__dirname, "lib/main.ts"),
