fbc49db1769b06053f24d571b1457e3846b7bef6
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-30 19:16:28 +0200

    added: Node definition

---
+++ lib/types/Node.ts
@@ -0,0 +1,47 @@
+export interface D3Node {
+	id: string;
+	x?: number;
+	y?: number;
+	vx?: number;
+	vy?: number;
+	fx?: number;
+	fy?: number;
+}
+
+export interface Shape {
+	type: string;
+	scale: number;
+	// template ref
+}
+
+export interface Spawn {
+	source: string | Node;
+	angle: number;
+	distance: number;
+}
+
+export enum AnchorType {
+	Soft = "soft",
+	Hard = "hard",
+}
+
+export interface Anchor {
+	type: AnchorType;
+	x: number;
+	y: number;
+}
+
+export interface Satellite {
+	// type?
+	source: string | Node;
+	angle: number;
+	distance: number;
+}
+
+export interface Node extends D3Node {
+	shape: Shape;
+	spawn?: Spawn;
+	anchor?: Anchor;
+	satellites?: Satellite[];
+	payload?: any;
+}
