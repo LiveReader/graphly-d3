bfd8db7154643874a46a035b4f22c416c1a45142
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-31 22:06:26 +0200

    updated: Template definition

--- lib/types/Template.ts
+++ lib/types/Template.ts
@@ -3,10 +3,10 @@ import { Node } from "./Node";
 
 export interface Template {
 	shapeSize: number;
-	shape: (
+	shapeBuilder: (
 		data: Node,
 		initialShape: d3.Selection<SVGElement, any, any, any> | null,
-		changes: Node,
+		changes: Node | null,
 		TemplateAPI: any
 	) => d3.Selection<SVGElement, any, any, any>;
 }
