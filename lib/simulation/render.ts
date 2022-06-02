c1b57bed922c0a1494753fd385b5728668406ca2
Jason Rietzke <jason.rietzke@livereader.com>
2022-06-03 00:28:08 +0200

    added: node spawning

--- lib/simulation/render.ts
+++ lib/simulation/render.ts
@@ -11,6 +11,7 @@ export async function renderNodes(this: ForceSimulation, graph: Graph) {
 	graph.nodes.forEach((node: Node) => {
 		node.forceSimulation = this;
 	});
+	spawnNodes(graph.nodes);
 
 	const nodeShapes = this.selectionGroups.nodes
 		.selectAll("[data-object='node']")
@@ -43,5 +44,31 @@ async function getNodeTemplates(graph: Graph) {
 		await TemplateStore.get(node).then((template: Template) => {
 			node.shape.template = template;
 		});
+	}
+}
+
+function spawnNodes(nodes: Node[]) {
+	for (let i in nodes) {
+		const node = nodes[i];
+		if (!node.x && !node.y) continue;
+		if (!node.fx && !node.fy) continue;
+		if (node.anchor?.x || node.anchor?.y) continue;
+		if (node.satellite?.x || node.satellite?.y) continue;
+		if (node.spawn) {
+			const sourceID = typeof node.spawn.source === "string" ? node.spawn.source : node.spawn.source.id;
+			const sourceNode = nodes.find((n: Node) => n.id === sourceID);
+			const sourcePos = {
+				x: sourceNode?.x ?? sourceNode?.fx ?? sourceNode?.anchor?.x ?? 0,
+				y: sourceNode?.y ?? sourceNode?.fy ?? sourceNode?.anchor?.y ?? 0,
+			};
+			const distance = node.spawn.distance;
+			const angle = node.spawn.angle;
+			const pos = {
+				x: sourcePos.x + distance * Math.cos((angle * Math.PI) / 180 - Math.PI / 2),
+				y: sourcePos.y + distance * Math.sin((angle * Math.PI) / 180 - Math.PI / 2),
+			};
+			node.x = pos.x;
+			node.y = pos.y;
+		}
 	}
 }
