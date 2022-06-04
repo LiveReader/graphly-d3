9a9de3449f802028365791cd8e440a7f82ebf58f
Jason Rietzke <jason.rietzke@livereader.com>
2022-06-04 15:55:10 +0200

    added: new link dragging

--- lib/simulation/drag.ts
+++ lib/simulation/drag.ts
@@ -7,7 +7,11 @@ export interface DraggableNode extends Node {
 	isDraged?: boolean;
 }
 
-export function dragNode(this: ForceSimulation): d3.DragBehavior<Element, unknown, unknown> {
+let newLinkDraging = false;
+let newLink: d3.Selection<SVGGElement, any, any, any>;
+let dragOffset: { x: number; y: number } = { x: 0, y: 0 };
+
+export function dragNode(this: ForceSimulation): d3.DragBehavior<Element, any, any> {
 	return d3
 		.drag()
 		.on("start", (e, d) => dragStart.bind(this)(e, d as DraggableNode))
@@ -15,7 +19,8 @@ export function dragNode(this: ForceSimulation): d3.DragBehavior<Element, unknow
 		.on("end", (e, d) => dragEnd.bind(this)(e, d as DraggableNode));
 }
 
-function dragStart(this: ForceSimulation, e: d3.D3DragEvent<Element, unknown, unknown>, d: DraggableNode) {
+function dragStart(this: ForceSimulation, e: d3.D3DragEvent<Element, any, any>, d: DraggableNode) {
+	if (e.sourceEvent.altKey) return dragNewLinkStart.bind(this)(e, d);
 	if (!this.draggableNodes) return;
 	this.simulation.alphaTarget(0.05).restart();
 	d.isDraged = true;
@@ -23,16 +28,58 @@ function dragStart(this: ForceSimulation, e: d3.D3DragEvent<Element, unknown, un
 	d.fy = e.y;
 }
 
-function dragMove(this: ForceSimulation, e: d3.D3DragEvent<Element, unknown, unknown>, d: DraggableNode) {
+function dragMove(this: ForceSimulation, e: d3.D3DragEvent<Element, any, any>, d: DraggableNode) {
+	if (newLinkDraging) return dragNewLinkMove.bind(this)(e, d);
 	if (!this.draggableNodes) return;
 	d.fx = e.x;
 	d.fy = e.y;
 }
 
-function dragEnd(this: ForceSimulation, e: d3.D3DragEvent<Element, unknown, unknown>, d: DraggableNode) {
+function dragEnd(this: ForceSimulation, e: d3.D3DragEvent<Element, any, any>, d: DraggableNode) {
+	if (newLinkDraging) return dragNewLinkEnd.bind(this)(e, d);
+	if (!this.draggableNodes) return dragNewLinkEnd.bind(this)(e, d);
 	if (!this.draggableNodes) return;
 	this.simulation.alphaTarget(0);
 	d.isDraged = false;
 	d.fx = null;
 	d.fy = null;
+}
+
+function dragNewLinkStart(this: ForceSimulation, e: d3.D3DragEvent<Element, any, any>, d: DraggableNode) {
+	dragOffset = {
+		x: e.sourceEvent.offsetX / this.worldTransform.k - this.worldTransform.x / this.worldTransform.k - (d.x ?? 0),
+		y: e.sourceEvent.offsetY / this.worldTransform.k - this.worldTransform.y / this.worldTransform.k - (d.y ?? 0),
+	};
+	newLinkDraging = true;
+	newLink = this.selectionGroups.links.append("g").attr("data-object", "prelink");
+	newLink
+		.append("line")
+		.attr("data-object", "prelink-line")
+		.attr("x1", d.x ?? 0)
+		.attr("y1", d.y ?? 0)
+		.attr("x2", e.x)
+		.attr("y2", e.y);
+}
+
+function dragNewLinkMove(this: ForceSimulation, e: d3.D3DragEvent<Element, any, any>, d: DraggableNode) {
+	const mousePos: { x: number; y: number } = {
+		x: e.x + dragOffset.x,
+		y: e.y + dragOffset.y,
+	};
+	newLink.select("[data-object='prelink-line']").attr("x2", mousePos.x).attr("y2", mousePos.y);
+}
+
+function dragNewLinkEnd(this: ForceSimulation, e: d3.D3DragEvent<Element, any, any>, d: DraggableNode) {
+	newLink.remove();
+	newLinkDraging = false;
+	let target = e.sourceEvent.target;
+	if (target == this.svgElement) return;
+	while (target.attributes["data-object"] == null) {
+		if (target == null) return;
+		target = target.parentElement;
+		if (!target) return;
+	}
+	const targetNode = this.graph.nodes.find((n: Node) => n.id == target.attributes["data-id"].value);
+	if (!targetNode) return;
+	if (targetNode.id == d.id) return;
 }
