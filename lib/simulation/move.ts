0b625435d8e2f3de209df9326613c51f8b4bc685
Jason Rietzke <jason.rietzke@livereader.com>
2022-06-06 23:07:30 +0200

    updated: move methods

--- lib/simulation/move.ts
+++ lib/simulation/move.ts
@@ -25,91 +25,79 @@ export interface MoveOptions {
 
 export function moveTo(this: ForceSimulation, options: MoveOptions, onZoom: (t: Transform) => void) {
 	if (options.transform) {
-		moveToTransform.bind(this)(options, onZoom);
-	} else if (options.boundaries) {
-		moveToBoundaries.bind(this)(options, onZoom);
-	} else if (options.nodes) {
-		moveToNodes.bind(this)(options, onZoom);
+		return moveToTransform.bind(this)(options.transform, onZoom, options.duration);
 	}
-}
-
-function moveToTransform(this: ForceSimulation, options: MoveOptions, onZoom: (t: Transform) => void) {
-	const value = options.transform;
-	if (!value) return;
-	const svgWidth = this.svgElement.clientWidth;
-	const svgHeight = this.svgElement.clientHeight;
-	const targetTransform: Transform = {
-		x: -value.x + svgWidth / 2 + (svgWidth / 2) * (1 - value.k) * (value.x / (svgWidth / 2)),
-		y: -value.y + svgHeight / 2 + (svgWidth / 2) * (1 - value.k) * (value.y / (svgHeight / 2)),
-		k: value.k,
-	};
-	transform.bind(this)(targetTransform, onZoom, options.duration);
-}
-
-function moveToBoundaries(this: ForceSimulation, options: MoveOptions, onZoom: (t: Transform) => void) {
-	const value = options.boundaries;
-	if (!value) return;
-	let xMin: number = Infinity;
-	let xMax: number = -Infinity;
-	let yMin: number = Infinity;
-	let yMax: number = -Infinity;
-	for (const b of value) {
-		xMin = Math.min(xMin, b.x);
-		xMax = Math.max(xMax, b.x + b.width);
-		yMin = Math.min(yMin, b.y);
-		yMax = Math.max(yMax, b.y + b.height);
+	const boundaries: Boundary[] = [];
+	if (options.boundaries) {
+		boundaries.push(cumulatedBoundary(options.boundaries));
 	}
-	const containerBounds: Boundary = {
-		x: xMin - (options.padding ?? 0),
-		y: yMin - (options.padding ?? 0),
-		width: xMax - xMin + (options.padding ?? 0) * 2,
-		height: yMax - yMin + (options.padding ?? 0) * 2,
-	};
-
-	const svgWidth = this.svgElement.clientWidth;
-	const svgHeight = this.svgElement.clientHeight;
-	const svgAspectRatio = svgWidth / svgHeight;
-	const boundaryAspectRatio = containerBounds.width / containerBounds.height;
-	const scale =
-		svgAspectRatio < boundaryAspectRatio ? svgWidth / containerBounds.width : svgHeight / containerBounds.height;
-	const targetTransform: Transform = {
-		x: containerBounds.x + containerBounds.width / 2,
-		y: containerBounds.y + containerBounds.height / 2,
-		k: scale,
-	};
-	moveToTransform.bind(this)(
-		{
-			transform: targetTransform,
-			duration: options.duration,
-		},
-		onZoom
-	);
+	if (options.nodes) {
+		boundaries.push(nodesBoundary(options.nodes));
+	}
+	moveToBoundary.bind(this)(cumulatedBoundary(boundaries), onZoom, options.duration, options.padding);
 }
 
-function moveToNodes(this: ForceSimulation, options: MoveOptions, onZoom: (t: Transform) => void) {
-	const value = options.nodes;
-	if (!value) return;
-	const nodes = this.graph.nodes.filter((n) => value.includes(n));
+function cumulatedBoundary(boundaries: Boundary[]): Boundary {
+	let minX: number = boundaries.reduce((acc, b) => Math.min(acc, b.x), Infinity);
+	let minY: number = boundaries.reduce((acc, b) => Math.min(acc, b.y), Infinity);
+	let maxX: number = boundaries.reduce((acc, b) => Math.max(acc, b.x + b.width), -Infinity);
+	let maxY: number = boundaries.reduce((acc, b) => Math.max(acc, b.y + b.height), -Infinity);
+	return {
+		x: minX,
+		y: minY,
+		width: maxX - minX,
+		height: maxY - minY,
+	};
+}
+
+function nodesBoundary(nodes: Node[]): Boundary {
 	const bounds = d3.extent(nodes, (n) => n.x);
 	const x = d3.extent(nodes, (n) => n.y);
 	const width = (bounds[1] ?? 0) - (bounds[0] ?? 0);
 	const height = (x[1] ?? 0) - (x[0] ?? 0);
 	const largestNodeSize = d3.max(nodes.map((n) => n.shape.scale * (n.shape.template?.shapeSize ?? 300))) ?? 0;
-
-	const targetBoundary: Boundary = {
+	return {
 		x: (bounds[0] ?? 0) - largestNodeSize / 2,
 		y: (x[0] ?? 0) - largestNodeSize / 2,
 		width: width + largestNodeSize,
 		height: height + largestNodeSize,
 	};
-	moveToBoundaries.bind(this)(
-		{
-			boundaries: [targetBoundary],
-			duration: options.duration,
-			padding: options.padding,
-		},
-		onZoom
-	);
+}
+
+function moveToTransform(this: ForceSimulation, t: Transform, onZoom: (t: Transform) => void, duration?: number) {
+	const svgWidth = this.svgElement.clientWidth;
+	const svgHeight = this.svgElement.clientHeight;
+	const targetTransform: Transform = {
+		x: -t.x + svgWidth / 2 + (svgWidth / 2) * (1 - t.k) * (t.x / (svgWidth / 2)),
+		y: -t.y + svgHeight / 2 + (svgWidth / 2) * (1 - t.k) * (t.y / (svgHeight / 2)),
+		k: t.k,
+	};
+	transform.bind(this)(targetTransform, onZoom, duration);
+}
+
+function moveToBoundary(
+	this: ForceSimulation,
+	boundary: Boundary,
+	onZoom: (t: Transform) => void,
+	duration?: number,
+	padding?: number
+) {
+	const svgWidth = this.svgElement.clientWidth;
+	const svgHeight = this.svgElement.clientHeight;
+	const svgAspectRatio = svgWidth / svgHeight;
+	const boundaryAspectRatio = (boundary.width + (padding ?? 0) * 2) / (boundary.height + (padding ?? 0) * 2);
+	const scale =
+		svgAspectRatio < boundaryAspectRatio
+			? svgWidth / (boundary.width + (padding ?? 0) * 2)
+			: svgHeight / (boundary.height + (padding ?? 0) * 2);
+	const targetTransform: Transform = {
+		x: boundary.x * scale + boundary.width * scale / 2 - (padding ?? 0),
+		y: boundary.y * scale + boundary.height * scale / 2 - (padding ?? 0),
+		// x: boundary.x + boundary.width / 2 - (padding ?? 0),
+		// y: boundary.y + boundary.height / 2 - (padding ?? 0),
+		k: scale,
+	};
+	moveToTransform.bind(this)(targetTransform, onZoom, duration);
 }
 
 function transform(this: ForceSimulation, t: Transform, onZoom: (t: Transform) => void, duration?: number) {
