6930b77cb82887d3b56aa09707255942e4db50f6
Jason Rietzke <jason.rietzke@livereader.com>
2022-06-10 22:40:25 +0200

    updated: theme config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,42 +5,41 @@ module.exports = {
 	title: "Graphly D3 Docs",
 	description: "Documentation for the Graphly D3 library",
 	themeConfig: {
-		repo: "livereader/graphly-d3-docs",
-		docsDir: "docs",
-		docsBranch: "main",
-		editLinks: true,
-		editLinkText: "Edit this page on GitHub",
-		lastUpdated: "Last Updated",
-
+		siteTitle: "Graphly D3",
+		logo: "/icons/graphly-d3-icon-dark-round.svg",
 		nav: [
 			{ text: "Introduction", link: "/" },
 			{ text: "Tutorial", link: "/tutorial/" },
 			{ text: "Data Structure", link: "/data-structure/" },
 			{ text: "Simulation API", link: "/simulation-api/" },
 			{ text: "Template API", link: "/template-api/" },
+			{
+				text: "LiveReader",
+				items: [
+					{
+						items: [
+							{ text: "Home Page", link: "https://livereader.com/" },
+							{ text: "Legal Notice", link: "https://livereader.com/legal-notice" },
+							{ text: "Privacy Policy", link: "https://livereader.com/privacy-policy" },
+						],
+					},
+				],
+			},
+		],
+		socialLinks: [
+			{ icon: "twitter", link: "https://twitter.com/LiveReaderCom" },
+			{ icon: "linkedin", link: "https://www.linkedin.com/company/livereader-gmbh" },
+			{ icon: "github", link: "https://github.com/livereader/graphly-d3" },
 		],
 		sidebar: {
 			"/tutorial/": sidebarItem("Tutorial", "/tutorial/"),
 			"/data-structure/": sidebarItem("Data Structure", "/data-structure/"),
 			"/simulation-api/": sidebarItem("Simulation API", "/simulation-api/"),
 			"/template-api/": sidebarItem("Template API", "/template-api/"),
-			"/": [
-				{
-					text: "Graphly D3",
-					children: [
-						{
-							index: 0,
-							text: "Introduction",
-							link: "/",
-						},
-						{
-							index: 0,
-							text: "Playground",
-							link: "/playground",
-						},
-					],
-				},
-			],
+		},
+		footer: {
+			message: "Graphly D3 Documentation",
+			copyright: `Copyright © ${new Date().getFullYear()} LiveReader GmbH`,
 		},
 	},
 };
@@ -48,26 +47,24 @@ module.exports = {
 function sidebarItem(title, route) {
 	const path = __dirname + "/.." + route;
 	const files = fs.readdirSync(path);
-
-	const children = [];
+	const items = [];
 	for (const file of files) {
 		if (fs.statSync(path + "/" + file).isDirectory() || !file.endsWith(".md")) return;
 		const fileName = file.split(".")[0];
 		const index = fileName === "index" ? -1 : 0;
 		const words = file.split(".")[0].split("_");
 		const title = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
-		children.push({
-			index: index,
+		items.push({
+			index,
 			text: fileName === "index" ? "Introduction" : title,
 			link: route + (fileName === "index" ? "" : fileName),
 		});
 	}
-	children.sort((a, b) => a.index - b.index);
-
+	items.sort((a, b) => a.index - b.index);
 	return [
 		{
 			text: title,
-			children: children,
+			items: items,
 		},
 	];
 }
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,4 @@
 import DefaultTheme from "vitepress/theme";
-import Layout from "../../components/Layout.vue";
 import "./theme.css";
 
-export default {
-	...DefaultTheme,
-	Layout: Layout,
-};
+export default DefaultTheme;
