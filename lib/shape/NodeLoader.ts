3abf1b757399f9b7aa480d45ea827d1ae490d544
Jason Rietzke <jason.rietzke@livereader.com>
2022-07-24 17:56:05 +0200

    implemented: body point calculation

--- lib/shape/NodeLoader.ts
+++ lib/shape/NodeLoader.ts
@@ -36,11 +36,45 @@ export default function Node(this: any, data: Node) {
 		return throwError(e.message);
 	}
 
+	const bbox = Shape.getBBox(nodeShape);
+	const shapeSize = data.shape.scale * (data.shape.template?.shapeSize ?? 300);
+	data.shape.bodyPoints = [];
+	const glyBody = nodeShape.select(".gly-body").node() as SVGPathElement;
+	const points = [];
+	if (glyBody) {
+		let n = data.shape?.bodyResolution || 32;
+		const totalLength = glyBody.getTotalLength();
+		for (let i = 0; i < n; i++) {
+			const p = glyBody.getPointAtLength((i / n) * totalLength);
+			p.x = Math.round(p.x);
+			p.y = Math.round(p.y);
+			points.push(p);
+		}
+		const pointsXmin = Math.min(...points.map((p) => p.x));
+		const pointsXmax = Math.max(...points.map((p) => p.x));
+		const pointsYmin = Math.min(...points.map((p) => p.y));
+		const pointsYmax = Math.max(...points.map((p) => p.y));
+		const pointsWidth = pointsXmax - pointsXmin;
+		const pointsHeight = pointsYmax - pointsYmin;
+		const pointsScale = {
+			x: bbox.width / pointsWidth,
+			y: bbox.height / pointsHeight,
+		};
+		points.forEach((p) => {
+			p.x = (p.x - pointsXmin) * pointsScale.x + bbox.x - (pointsWidth * pointsScale.x) / 2 + shapeSize;
+			p.y = (p.y - pointsYmin) * pointsScale.y + bbox.y - (pointsHeight * pointsScale.y) / 2 + shapeSize;
+		});
+		data.shape.bodyPoints = points;
+	}
+
 	data.shape.failed = false;
-	Shape.transform(
-		nodeShape.select("[data-object=shape]"),
-		data.shape.scale * (data.shape.template?.shapeSize ?? 300)
-	);
+	const transform = Shape.transform(nodeShape.select("[data-object=shape]"), shapeSize);
+
+	for (let p of points) {
+		p.x = (p.x - shapeSize) * transform.scale;
+		p.y = (p.y - shapeSize) * transform.scale;
+	}
+
 	return nodeShape.node();
 
 	function throwError(this: any, message: string) {
