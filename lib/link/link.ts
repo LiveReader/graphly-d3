44f9968be6ab454b52ba1a5a04d9cbc04247e595
Jason Rietzke <jason.rietzke@livereader.com>
2022-07-24 18:08:23 +0200

    implemented: surface point intersection calculation

--- lib/link/link.ts
+++ lib/link/link.ts
@@ -1,4 +1,6 @@
+import * as d3 from "d3";
 import * as Shape from "../shape/shape";
+import { linkID } from "../simulation/render";
 import { Node } from "../types/Node";
 import { Link } from "../types/Link";
 
@@ -142,14 +144,20 @@ function getSurfacePoints(
 		.attr("d", `M ${start.x} ${start.y}` + `Q ${center.x}, ${center.y}` + ` ${end.x} ${end.y}`)
 		.node();
 	if (!path) return { start, end, center: off };
+
 	const sourceSize = link.source.shape.failed
 		? link.source.forceSimulation?.templateStore.errorTemplate.shapeSize
 		: link.source.shape.template?.shapeSize ?? 300;
 	const targetSize = link.target.shape.failed
 		? link.target.forceSimulation?.templateStore.errorTemplate.shapeSize
 		: link.target.shape.template?.shapeSize ?? 300;
-	const startDistance = (link.source.shape.scale ?? 1) * ((sourceSize ?? 300) / 2);
-	const endDistance = (link.target.shape.scale ?? 1) * ((targetSize ?? 300) / 2);
+
+	const startIntersection = calculateIntersectionDistance(link.source, link.target, link, false);
+	const endIntersection = calculateIntersectionDistance(link.source, link.target, link, true);
+
+	const startDistance = startIntersection || (link.source.shape.scale ?? 1) * ((sourceSize ?? 300) / 2);
+	const endDistance = endIntersection || (link.target.shape.scale ?? 1) * ((targetSize ?? 300) / 2); // TODO end not working!
+
 	const surfaceStart = path.getPointAtLength(startDistance + distance);
 	const surfaceEnd = path.getPointAtLength(path.getTotalLength() - endDistance - distance - arrowDistance);
 	const surfaceOffset = offset(link, surfaceStart, surfaceEnd, 0.1);
@@ -159,4 +167,86 @@ function getSurfacePoints(
 		center: { x: surfaceCenter.x, y: surfaceCenter.y },
 		end: { x: surfaceEnd.x, y: surfaceEnd.y },
 	};
+}
+
+function calculateIntersectionDistance(
+	sourceNode: Node,
+	targetNode: Node,
+	link: Link,
+	reversed: boolean = false
+): number {
+	const linePath: SVGPathElement = d3
+		.select("[data-id='" + linkID(link) + "']")
+		.select("[data-object='link-line-full']")
+		.node() as SVGPathElement;
+	if (!linePath) return 0;
+
+	const bodyPoints = sourceNode.shape.bodyPoints ?? [];
+	const lineLength = linePath.getTotalLength();
+
+	const stepSize = reversed
+		? -((targetNode.shape.template?.shapeSize ?? 300) * targetNode.shape.scale) / 20
+		: ((sourceNode.shape.template?.shapeSize ?? 300) * sourceNode.shape.scale) / 20;
+	let length = reversed ? lineLength : 0;
+	let globalPoint = linePath.getPointAtLength(length);
+	let localPoint = { x: 0, y: 0 };
+
+	if (!reversed) {
+		localPoint = {
+			x: globalPoint.x - (sourceNode.x ?? 0),
+			y: globalPoint.y - (sourceNode.y ?? 0),
+		};
+	} else {
+		localPoint = {
+			x: globalPoint.x - (targetNode.x ?? 0),
+			y: globalPoint.y - (targetNode.y ?? 0),
+		};
+	}
+
+	while (pointInPolygon(localPoint, bodyPoints) && length <= lineLength) {
+		length += stepSize;
+		globalPoint = linePath.getPointAtLength(length);
+		if (!reversed) {
+			localPoint = {
+				x: globalPoint.x - (sourceNode.x ?? 0),
+				y: globalPoint.y - (sourceNode.y ?? 0),
+			};
+		} else {
+			localPoint = {
+				x: globalPoint.x - (targetNode.x ?? 0),
+				y: globalPoint.y - (targetNode.y ?? 0),
+			};
+		}
+	}
+	while (!pointInPolygon(localPoint, bodyPoints) && length >= 0) {
+		length = reversed ? length + 1 : length - 1;
+		globalPoint = linePath.getPointAtLength(length);
+		if (!reversed) {
+			localPoint = {
+				x: globalPoint.x - (sourceNode.x ?? 0),
+				y: globalPoint.y - (sourceNode.y ?? 0),
+			};
+		} else {
+			localPoint = {
+				x: globalPoint.x - (targetNode.x ?? 0),
+				y: globalPoint.y - (targetNode.y ?? 0),
+			};
+		}
+	}
+	const distance = reversed ? lineLength - length : length;
+	return distance;
+}
+
+function pointInPolygon(point: { x: number; y: number }, polygon: { x: number; y: number }[]) {
+	var inside = false;
+	for (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+		var xi = polygon[i].x,
+			yi = polygon[i].y;
+		var xj = polygon[j].x,
+			yj = polygon[j].y;
+
+		var intersect = yi > point.y != yj > point.y && point.x < ((xj - xi) * (point.y - yi)) / (yj - yi) + xi;
+		if (intersect) inside = !inside;
+	}
+	return inside;
 }
