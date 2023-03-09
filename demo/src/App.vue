64c6d1afa811bb6a5b9afe28c915f2390f1cdf7c
Jason Rietzke <jason.rietzke@livereader.com>
2023-03-09 13:25:18 +0100

    add demo config elements

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -11,10 +11,47 @@
 			@theme-change="themeChange"
 		/>
 	</div>
+	<div v-if="showConfig" class="config" :class="{ dark: theme == 'dark' }">
+		<svg-icon
+			style="cursor: pointer; display: block; margin: auto; margin-right: 0"
+			type="mdi"
+			:path="mdiThemeLightDark"
+			:color="theme == 'dark' ? 'white' : 'black'"
+			:size="24"
+			@click="theme = theme == 'dark' ? 'light' : 'dark'"
+		/>
+		<div class="group">
+			<h3>Settings</h3>
+			<input v-model="padding" type="number" placeholder="padding" />
+			<input v-model="duration" type="number" placeholder="duration (ms)" />
+		</div>
+		<div class="group">
+			<h3>Move</h3>
+			<input v-model="transformX" type="number" placeholder="x" class="third" />
+			<input v-model="transformY" type="number" placeholder="y" class="third" />
+			<input v-model="transformK" type="number" placeholder="k" class="third" />
+			<button @click="moveToTransform">Move</button>
+		</div>
+		<div class="group">
+			<h3>Bounds</h3>
+			<input v-model="boundsX" type="number" placeholder="x" class="half" />
+			<input v-model="boundsY" type="number" placeholder="y" class="half" />
+			<input v-model="boundsW" type="number" placeholder="width" class="half" />
+			<input v-model="boundsH" type="number" placeholder="height" class="half" />
+			<button @click="moveToBounds">Move</button>
+		</div>
+		<div class="group">
+			<h3>Utils</h3>
+			<button @click="showNodes">Show Nodes</button>
+			<button @click="includeNodes">Include Nodes</button>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed, onMounted } from "vue";
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiThemeLightDark } from "@mdi/js";
 import { Event, ForceSimulation, Graph, Node } from "@livereader/graphly-d3";
 import GraphlyD3 from "@livereader/graphly-d3/component/vue3";
 import "@livereader/graphly-d3/style.css";
@@ -28,7 +65,22 @@ let graph: Graph = {
 	nodes: [],
 	links: [],
 };
+const showConfig = ref(false);
+const theme = ref("dark");
+const padding = ref(0);
+const duration = ref(1000);
+
+const transformX = ref(0);
+const transformY = ref(0);
+const transformK = ref(1);
+
+const boundsX = ref(0);
+const boundsY = ref(0);
+const boundsW = ref(0);
+const boundsH = ref(0);
+
 onMounted(() => {
+	showConfig.value = true;
 	simulation.value.templateStore.add("demo_template", DemoTemplate as any);
 	graph = demoData.graph as unknown as Graph;
 	simulation.value.render(graph);
@@ -67,4 +119,44 @@ function themeChange(theme: "light" | "dark") {
 	console.log(theme);
 }
 
+function moveToTransform() {
+	simulation.value.moveTo({
+		padding: padding.value,
+		duration: duration.value,
+		transform: {
+			x: transformX.value,
+			y: transformY.value,
+			k: transformK.value,
+		},
+	});
+}
+function moveToBounds() {
+	simulation.value.moveTo({
+		padding: padding.value,
+		duration: duration.value,
+		boundaries: [
+			{
+				x: boundsX.value,
+				y: boundsY.value,
+				width: boundsW.value,
+				height: boundsH.value,
+			},
+		],
+	});
+}
+function showNodes() {
+	simulation.value.moveTo({
+		padding: padding.value,
+		duration: duration.value,
+		nodes: graph.nodes,
+	});
+}
+function includeNodes() {
+	simulation.value.moveTo({
+		padding: padding.value,
+		duration: duration.value,
+		boundaries: [simulation.value.worldBounds],
+		nodes: graph.nodes,
+	});
+}
 </script>
