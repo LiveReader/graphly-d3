f404bcc1f96ea9982722b9f17ffa60d136ee1065
Jason Rietzke <jason.rietzke@livereader.com>
2023-05-04 19:27:14 +0200

    feat: expose Template type

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,10 @@
 import ForceSimulation from "./simulation/forceSimulation";
 import { Event } from "./simulation/eventStore";
 import * as TemplateAPI from "./templateAPI";
+import { Template } from "./types/Template";
 
 export { ForceSimulation, Event, TemplateAPI };
+export type { Template };
 export * from "./types/Graph";
 export * from "./types/Node";
 export * from "./types/Link";
