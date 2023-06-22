e1ccd87f9cb6b2f2c8dca137ef2f086ec2169fed
Jason Rietzke <jason.rietzke@livereader.com>
2023-06-22 23:06:35 +0200

    feat: Template interface typed TemplateAPI injection

--- lib/types/Template.ts
+++ lib/types/Template.ts
@@ -1,8 +1,9 @@
 import * as d3 from "d3";
 import { Node } from "./Node";
+import type * as TAPI from "../templateAPI";
 
 export interface Template<T = any> {
 	shapeSize: number;
 	shapePayload?: T;
-	shapeBuilder: (data: Node<T>, TemplateAPI: any) => d3.Selection<any, any, any, any>;
+	shapeBuilder: (data: Node<T>, TemplateAPI: typeof TAPI) => d3.Selection<any, any, any, any>;
 }
