2be0419f0ece68f0b599e07fe0658c9f0835587e
Jason Rietzke <jason.rietzke@livereader.com>
2023-09-03 16:28:58 +0200

    chore: vite build type definitions

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
-import { defineConfig } from "vite";
+import fs from "fs";
+import path from "path";
+import { defineConfig, type Plugin } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			"@": resolve(__dirname, "lib"),
-		},
-	},
+	plugins: [vue(), dts({ insertTypesEntry: true, rollupTypes: true }), mdts()],
 	build: {
 		lib: {
 			entry: {
-				main: resolve(__dirname, "lib/main.ts"),
-				"component-vue3": resolve(__dirname, "components/vue3/index.ts"),
+				main: path.resolve(__dirname, "lib/main.ts"),
+				"component-vue3": path.resolve(__dirname, "components/vue3/index.ts"),
 			},
 			name: "Graphly D3",
-			fileName: (format, name) => `${name}.${format}.js`,
+			formats: ["es", "cjs"]
 		},
 		minify: false,
 		rollupOptions: {
@@ -31,3 +28,24 @@ export default defineConfig({
 		},
 	},
 });
+
+function mdts(): Plugin {
+	let distPath: string;
+	return {
+		name: "vite:dmts",
+		configResolved(getResolvedConfig) {
+			const resolvedConfig = getResolvedConfig;
+			distPath = path.join(resolvedConfig.root, resolvedConfig.build.outDir, resolvedConfig.base);
+		},
+		writeBundle() {
+			const files = fs.readdirSync(distPath);
+			for (const file of files) {
+				if (file.endsWith(".d.ts")) {
+					const filePath = path.join(distPath, file);
+					const content = fs.readFileSync(filePath, "utf-8");
+					fs.writeFileSync(filePath.replace(".d.ts", ".d.mts"), content);
+				}
+			}
+		},
+	};
+}
